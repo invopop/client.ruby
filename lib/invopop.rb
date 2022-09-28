e80cefb6e388196796f588a598f3c021cd406d77
Luismi Cavallé <luismi@lmcavalle.com>
2022-09-28 10:49:19 +0000

    Generate barebones gem with `bundle gem`

---
+++ lib/invopop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "invopop/version"
+
+module Invopop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/invopop/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Invopop
+  VERSION = "0.1.0"
+end
