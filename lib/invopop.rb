c069e842ed0b2c999bfc3e46bb54d3a14ce2096d
Luismi Cavallé <luismi@lmcavalle.com>
2022-10-11 08:43:16 +0200

    Add custom errors to raise when API requests fail

--- lib/invopop.rb
+++ lib/invopop.rb
@@ -6,6 +6,8 @@ require 'hashme'
 
 require_relative 'invopop/client'
 require_relative 'invopop/connection'
+require_relative 'invopop/connection/error_manager'
+require_relative 'invopop/error'
 require_relative 'invopop/namespace'
 require_relative 'invopop/resource'
 require_relative 'invopop/silo'
